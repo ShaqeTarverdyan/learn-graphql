51d91e5e81f5310ddadee5a4a82562a8e8cc149e
Shaqe Tarverdyan <tshaqe@gmail.com>
2024-06-26 12:39:44 +0400

    get data with gql

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import './App.css'
+import { useQuery, gql } from "@apollo/client";
+import "./App.css"
 
-function App() {
+const USERS_QUERY = gql`
+  {
+    users {
+      list {
+        username
+        user_id
+      }
+      total
+    }
+  }
+`;
+
+export default function App() {
+  const { data, loading, error } = useQuery(USERS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
 
   return (
     <>
-    <div>test</div>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.users.list.map((user) => (
+            <tr key={user.user_id}>
+              <td>{user.user_id}</td>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>Total of users is {data.users.total}</h3>
     </>
-  )
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'https://parcella-service-v8w6x.ondigitalocean.app/graphql',
+  cache: new InMemoryCache(),
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+  <App />
+</ApolloProvider>,
 )
